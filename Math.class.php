58ecfc81388ca292a1433adfab6a36b04b6a955b
João Bolsson <joaovictorbolsson@gmail.com>
2018-07-14 20:43:45 -0300

    estimadores

--- Math.class.php
+++ Math.class.php
@@ -19,15 +19,7 @@ class Math {
         $n = count($x);
 
         // Sxx
-        for ($i = 0; $i < $n; $i++) {
-            $sum = 0;
-
-            for ($j = 0; $j < $n; $j++) {
-                $sum += floatval($x[$j]);
-            }
-
-            $Sxx += floatval(pow($x[$i], 2) - (pow($sum, 2) / $n));
-        }
+        $Sxx = self::getSxx($x);
 
         // Syy
         for ($i = 0; $i < $n; $i++) {
@@ -41,6 +33,21 @@ class Math {
         }
 
         // Sxy
+        $Sxy = self::getSxy($x, $y);
+
+        // estimador
+        $p = $Sxy / sqrt(floatval($Sxx * $Syy));
+
+        return $p;
+    }
+
+    public static function getSxy(array $x, array $y):float {
+        if (count($x) !== count($y)) {
+            echo "BASE DE DADOS COM TAMANHO DIFERENTE";
+            return 0; // erro
+        }
+        $Sxy = 0;
+        $n = count($x);
         for ($i = 0; $i < $n; $i++) {
             $sumX = 0;
             $sumY = 0;
@@ -51,17 +58,55 @@ class Math {
 
             $Sxy += floatval($x[$i] * $y[$i] - ($sumX * $sumY / $n));
         }
+        return $Sxy;
+    }
 
-        // estimador
-        $p = $Sxy / sqrt(floatval($Sxx * $Syy));
+    public static function getSxx(array $x):float {
+        $n = count($x);
+        $Sxx = 0;
+        for ($i = 0; $i < $n; $i++) {
+            $sum = 0;
 
-        return $p;
+            for ($j = 0; $j < $n; $j++) {
+                $sum += floatval($x[$j]);
+            }
+
+            $Sxx += floatval(pow($x[$i], 2) - (pow($sum, 2) / $n));
+        }
+
+        return $Sxx;
     }
 
     public static function testeHipoteseCorrelacao(float $p, int $n): float {
         $T0 = floatval($p * sqrt(floatval($n - 2)) / sqrt(floatval(1 - pow($p, 2))));
 
         return $T0;
+    }
+
+    public static function estimadorMinQuad(array $Y, array $x, int $index): float {
+        if (count($Y) !== count($x)) {
+            echo "ARRAYS COM TAMANHOS DIFERENTES";
+            return 0; // erro
+        }
+        $medY = 0;
+        $n = count($Y);
+        for ($i = 0; $i < $n; $i++) {
+            $medY += floatval($Y[$i]);
+        }
+        $medY /= $n;
+
+        $medX = 0;
+        for ($i = 0; $i < $n; $i++) {
+            $medX += floatval($x[$i]);
+        }
+        $medX /= $n;
+
+        $b1 = self::getSxy($x, $Y) / self::getSxx($x);
+        $b0 = $medY - $b1 * $medX;
+
+        $uYI = floatval($b0 + $b1 * $x[$index]);
+
+        return $uYI;
     }
 
 }
